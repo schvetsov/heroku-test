6d593acf4c22b1de455248f11c5b5775496143da
Stan Chvetsov <sac08xyz@gmail.com>
2019-02-22 12:03:00 -0500

    working

--- app.js
+++ app.js
@@ -39,4 +39,4 @@ app.get('/', (req, res) =>
 
 )
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}! ${fullname}`))
